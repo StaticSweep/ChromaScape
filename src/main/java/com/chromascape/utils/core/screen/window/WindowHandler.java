966bc6aed1153279cba8e263ca4d3266a9f2312f
evoke <evokeknife@gmail.com>
2025-09-06 21:34:53 -0700

    Cleanup hardcode

--- src/main/java/com/chromascape/utils/core/screen/window/WindowHandler.java
+++ src/main/java/com/chromascape/utils/core/screen/window/WindowHandler.java
@@ -14,7 +14,7 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 public class WindowHandler {
 
-  private static final String windowName = "RuneLite - Frostcast97";
+  private static final String windowName = "RuneLite";
 
   /**
    * JNA interface for accessing low-level Win32 User32 functions that are not provided by the
