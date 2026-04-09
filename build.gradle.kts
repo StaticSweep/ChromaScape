c4de3f4651678d8bb2d2561e64bb217e31d15fb9
Debs Gayen <debsgayen@gmail.com>
2026-04-09 18:23:35 +0100

    bump version (#61)
    Bump SemVer to 0.4.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.chromascape"
-version = "0.3.0"
+version = "0.4.0-SNAPSHOT"
 
 // Customize build directories - put DLLs in build/dist
 layout.buildDirectory.set(file("build"))
